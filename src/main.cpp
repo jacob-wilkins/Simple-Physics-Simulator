5c9ec1e4ed28601c4472c94bfa3ed991c5406c9d
Jake Wilkins <jacobwilkins2003@gmail.com>
2025-02-20 16:57:40 -0500

    working gravity, need bounds in window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
+#include "physics/environment.hpp"
 
 using namespace std;
 
@@ -8,25 +9,37 @@ int main()
     sf::ContextSettings settings;
     settings.antiAliasingLevel = 1;
 
-    sf::RenderWindow window(sf::VideoMode({1920u, 1080u}), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode({1920u, 1080u}), "Simple Physics Simulator");
 
     window.setPosition({0, 0});
     window.setVerticalSyncEnabled(true); // call it once after creating the window
     window.setFramerateLimit(60); // call it once after creating the window
 
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    Environment env(window);
+    env.addCircle(50.0f, {100.0f, 100.0f}, sf::Color::White);
 
     while (window.isOpen())
     {
         while (const optional event = window.pollEvent())
         {
-            if (event->is<sf::Event::Closed>())
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
+            }
+
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+                env.addCircle(50.0f, static_cast<sf::Vector2f>(sf::Mouse::getPosition(window)), sf::Color::White);
+            }
         }
 
         window.clear();
-        window.draw(shape);
+
+        env.update();
+
+        // Draw all circles in the environment
+        for (const auto& circle : env.getCircles()) {
+            window.draw(circle);
+        }
+
         window.display();
     }
 }
--- src/physics/environment.hpp
+++ src/physics/environment.hpp
@@ -1,9 +1,40 @@
+#include <vector>
+#include <cmath>
+#include <SFML/Graphics.hpp>
+
 class Environment {
     public:
         Environment() = default;
 
+        Environment(sf::RenderWindow& window) : window{window} {}
+
+        void addCircle(float radius, sf::Vector2f pos, sf::Color color) {
+            sf::CircleShape circle(radius);
+            circle.setPosition(pos);
+            circle.setFillColor(color);
+            objects.push_back(circle);
+        }
+
+        const std::vector<sf::CircleShape>& getCircles() const {
+            return objects;
+        }
+
         void update() {
             // update the positions of all objects
             // gravity, acceleration, position, etc
+
+            addGravity();
         }
-}
+
+    private:
+        std::vector<sf::CircleShape> objects;
+        sf::RenderWindow& window;
+        sf::Vector2u bounds = window.getSize();
+        sf::Vector2f gravity = {0, 19.6};
+
+        void addGravity(){
+            for (auto& object : objects) {
+                object.move(gravity);
+            }
+        }
+};
